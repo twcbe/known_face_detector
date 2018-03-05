fe6886c8c65d1e95f7b31780da5ee694cee5d18c
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-03-05 17:30:37 +0530

    Update deploy.sh to include only tracked files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -el
 
-tar cf - ./* | ssh tw@tw-ubuntu-01.local bash -xel -c "\"mkdir -p /home/tw/Projects/known_face_detector && rm -rf /home/tw/Projects/known_face_detector/* && cat - | tar xf - -C /home/tw/Projects/known_face_detector\""
+git archive --format=tar HEAD | ssh tw@tw-ubuntu-01.local bash -xel -c "\"mkdir -p /home/tw/Projects/known_face_detector && rm -rf /home/tw/Projects/known_face_detector/* && cat - | tar xf - -C /home/tw/Projects/known_face_detector\""
