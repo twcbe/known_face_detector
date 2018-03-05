d72c56abe38d0bbbfcdf20e7e229aef6084f8a1d
Nandhitha Kamal <knandhi@thoughtworks.com>
2018-03-05 13:38:46 +0530

    Add classifier file

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
+import pickle
+
 def load_file(filename, extension='pkl'):
     with open(filename+"."+extension, 'rb') as file_handle:
-        result = load(file_handle)
+        result = pickle.load(file_handle)
     return result
 
 def save_file(data, filename, extension='pkl'):
     with open(filename+"."+extension, 'rb') as file_handle:
-        save(data, file_handle)
+        pickle.dump(data, file_handle)
