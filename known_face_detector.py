5caa59a68e8aa216ada30ecd6f36e664dafc71ce
Nandhitha Kamal <knandhi@thoughtworks.com>
2018-03-12 15:15:20 +0530

    [Nandhitha] Added mqtt publisher

--- known_face_detector.py
+++ known_face_detector.py
@@ -7,12 +7,18 @@ import pickle
 from sklearn.naive_bayes import GaussianNB
 from utils import *
 import numpy as np
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+
 
 dlibFacePredictor = '/root/openface/models/dlib/shape_predictor_68_face_landmarks.dat' # TODO: possible perf improvement reduce landmarks
 scale = 2
 MIN_CONFIDENCE_THRESHOLD=0.9
 MAX_DISTANCE_THRESHOLD=0.5
-
+auth = {
+        'username':"socnhliq",
+        'password':"7wrIE_dxtWE6"
+    }
 class KnownFaceDetector():
     def __init__(self, trained_classifier_file="tw_coimbatore_faces_classifier", face_detector_class = DlibFaceDetector):
         start=time.time()
@@ -27,7 +33,27 @@ class KnownFaceDetector():
         self.reps = load_file("reps")
         self.clusters = load_file("clusters")
 
+<<<<<<< HEAD
     def identify_faces(self, image, max_faces=1):
+=======
+        print("Publishing event to subscriber")
+        auth = {
+            'username':"socnhliq",
+            'password':"7wrIE_dxtWE6"
+        }
+
+        publish.single("face/known",
+            payload = "Initialised",
+            hostname = "m11.cloudmqtt.com",
+            client_id = "publisher",
+            auth = auth,
+            port = 10833,
+            protocol = mqtt.MQTTv311)
+
+
+    def detect_faces(self, image, max_faces=1):
+        bounding_boxes = self.get_all_bounding_boxes(image)
+>>>>>>> [Nandhitha] Added mqtt publishing
         # returns [{known: false, bb: rect()}, {known: true, bb: rect(), representation: rep}]
         return [self.identify_face(image, bounding_box) for bounding_box in self.face_detector.detect_faces(image, max_faces)]
 
@@ -38,7 +64,7 @@ class KnownFaceDetector():
         # TODO: NEXT
         (predicted_class, nearest_class, distance, classifier_confidence) = self.predict_cluster(rep)
         # predicted_class = self.classifier.predict_proba([rep])[0]
-        return {
+        result =  {
             'known': predicted_class != None,
             'bb': bounding_box,
             'class': predicted_class,
@@ -48,6 +74,8 @@ class KnownFaceDetector():
             'landmarks': landmarks,
             'classifier_confidence': classifier_confidence
         }
+        publish_event("Face detected: Class - {}".format(predicted_class))
+        return result
 
     def predict_cluster(self, rep):
         probabilities = self.classifier.predict_proba([rep])[0]
@@ -61,6 +89,7 @@ class KnownFaceDetector():
                 return (predicted_cluster, predicted_cluster, distance, confidence)
             return (None, predicted_cluster, distance, confidence)
         return (None, None, None, np.count_nonzero(high_confidence))
+        
 
     def align_face(self, image, bounding_box):
         start=time.time()
@@ -98,3 +127,12 @@ def compute_mean_euclidean_distance(all_reps, clusters, face_rep, predicted_clus
 def distance_between(a,b):
     d = a.ravel() - b.ravel()
     return np.dot(d,d)
+
+def publish_event(result):
+    publish.single("face/known",
+        payload = result,
+        hostname = "m11.cloudmqtt.com",
+        client_id = "publisher",
+        auth = auth,
+        port = 10833,
+        protocol = mqtt.MQTTv311)
