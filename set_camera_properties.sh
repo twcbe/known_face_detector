a1192930160b1628844dd643f02aa9589c8f73a6
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-04-19 12:24:16 +0530

    [Ashok] Update ip of the new development/testing box

--- set_camera_properties.sh
+++ set_camera_properties.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -el
 
-v4l2-ctl -d0 -c backlight_compensation=1,sharpness=138,power_line_frequency=1,white_balance_temperature_auto=1,saturation=128,contrast=128,brightness=128,focus_auto=0,zoom_absolute=300
+v4l2-ctl -d1 -c backlight_compensation=1,sharpness=138,power_line_frequency=1,white_balance_temperature_auto=1,saturation=128,contrast=128,brightness=128,focus_auto=0,zoom_absolute=300
 echo "Camera parameters updated successfully!"
